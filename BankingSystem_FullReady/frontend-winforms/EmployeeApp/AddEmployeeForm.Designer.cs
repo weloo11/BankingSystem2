048d91eb358867e6da269df37651c00beb860b9e
karimmarwanw <kokoomar900@gmail.com>
2025-12-02 00:20:06 +0200

    fix

--- BankingSystem_FullReady/frontend-winforms/EmployeeApp/AddEmployeeForm.Designer.cs
+++ BankingSystem_FullReady/frontend-winforms/EmployeeApp/AddEmployeeForm.Designer.cs
@@ -1,13 +1,15 @@
 namespace EmployeeApp
 {
-    partial class EmployeeHomeForm
+    partial class AddEmployeeForm
     {
         private System.ComponentModel.IContainer components = null;
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing && components != null)
+            if (disposing && (components != null))
+            {
                 components.Dispose();
+            }
             base.Dispose(disposing);
         }
 
@@ -15,137 +17,147 @@ namespace EmployeeApp
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-
-            this.panelHeader = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.btnProfileMenu = new System.Windows.Forms.Button();
-
-            this.lblWelcome = new System.Windows.Forms.Label();
-            this.btnApproveAccounts = new System.Windows.Forms.Button();
-            this.btnApproveLoans = new System.Windows.Forms.Button();
-            this.btnApproveCertificates = new System.Windows.Forms.Button();
-            this.btnAddEmployee = new System.Windows.Forms.Button();
-
-            this.contextProfile = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.viewProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
-            // ===========================
-            // HEADER PANEL
-            // ===========================
-            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(40, 60, 90);
-            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelHeader.Height = 80;
-
-            // Title label
-            this.lblTitle.Text = "Employee Dashboard";
-            this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            // 
+            // lblTitle
+            // 
             this.lblTitle.AutoSize = true;
-
-            // Profile menu button
-            this.btnProfileMenu.Text = "Profile ▼";
-            this.btnProfileMenu.BackColor = System.Drawing.Color.FromArgb(60, 120, 200);
-            this.btnProfileMenu.ForeColor = System.Drawing.Color.White;
-            this.btnProfileMenu.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.btnProfileMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnProfileMenu.FlatAppearance.BorderSize = 0;
-            this.btnProfileMenu.Size = new System.Drawing.Size(180, 35);
-            this.btnProfileMenu.Anchor = (System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right);
-            this.btnProfileMenu.Location = new System.Drawing.Point(700, 20); // initial, moved at runtime
-            this.btnProfileMenu.Click += new System.EventHandler(this.btnProfileMenu_Click);
-
-            this.panelHeader.Controls.Add(this.lblTitle);
-            this.panelHeader.Controls.Add(this.btnProfileMenu);
-
-            // ===========================
-            // PROFILE CONTEXT MENU
-            // ===========================
-            this.contextProfile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-                this.viewProfileToolStripMenuItem,
-                this.logoutToolStripMenuItem
-            });
-
-            this.viewProfileToolStripMenuItem.Text = "View Profile";
-            this.viewProfileToolStripMenuItem.Click += new System.EventHandler(this.viewProfileToolStripMenuItem_Click);
-
-            this.logoutToolStripMenuItem.Text = "Logout";
-            this.logoutToolStripMenuItem.Click += new System.EventHandler(this.logoutToolStripMenuItem_Click);
-
-            // ===========================
-            // WELCOME LABEL
-            // ===========================
-            this.lblWelcome.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.lblWelcome.Location = new System.Drawing.Point(40, 110);
-            this.lblWelcome.AutoSize = true;
-
-            // ===========================
-            // MAIN BUTTONS (STYLE + POSITION)
-            // ===========================
-            SetupButton(this.btnApproveAccounts, "Approve Customer Accounts", 160);
-            this.btnApproveAccounts.Click += new System.EventHandler(this.btnApproveAccounts_Click);
-
-            SetupButton(this.btnApproveLoans, "Approve Loans", 230);
-            this.btnApproveLoans.Click += new System.EventHandler(this.btnApproveLoans_Click);
-
-            SetupButton(this.btnApproveCertificates, "Approve Certificates", 300);
-            this.btnApproveCertificates.Click += new System.EventHandler(this.btnApproveCertificates_Click);
-
-            SetupButton(this.btnAddEmployee, "Add New Employee", 370);
-            this.btnAddEmployee.Click += new System.EventHandler(this.btnAddEmployee_Click);
-
-            // ===========================
-            // FORM PROPERTIES
-            // ===========================
-            this.BackColor = System.Drawing.Color.FromArgb(230, 235, 245);
-            this.ClientSize = new System.Drawing.Size(900, 600);
-            this.Controls.Add(this.panelHeader);
-            this.Controls.Add(this.lblWelcome);
-            this.Controls.Add(this.btnApproveAccounts);
-            this.Controls.Add(this.btnApproveLoans);
-            this.Controls.Add(this.btnApproveCertificates);
-            this.Controls.Add(this.btnAddEmployee);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "EmployeeHomeForm";
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(80, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(155, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Add Employee";
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.AutoSize = true;
+            this.lblEmployeeID.Location = new System.Drawing.Point(30, 70);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(85, 15);
+            this.lblEmployeeID.TabIndex = 1;
+            this.lblEmployeeID.Text = "Employee ID:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(30, 110);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 15);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(30, 150);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(41, 15);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(30, 190);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(63, 15);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(130, 67);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(180, 23);
+            this.txtEmployeeID.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(130, 107);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 23);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(130, 147);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 23);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(130, 187);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(180, 23);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(60, 235);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 35);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Add";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(190, 235);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 35);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Close";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // AddEmployeeForm
+            // 
+            this.ClientSize = new System.Drawing.Size(340, 300);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblEmployeeID);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "AddEmployeeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Employee Dashboard";
-            this.Load += new System.EventHandler(this.EmployeeHomeForm_Load);
-
+            this.Text = "Add Employee";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        // Helper to style buttons consistently
-        private void SetupButton(System.Windows.Forms.Button button, string text, int top)
-        {
-            button.Text = text;
-            button.BackColor = System.Drawing.Color.FromArgb(60, 120, 200);
-            button.ForeColor = System.Drawing.Color.White;
-            button.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            button.FlatAppearance.BorderSize = 0;
-            button.Size = new System.Drawing.Size(280, 50);
-            button.Location = new System.Drawing.Point(20, top);
-        }
-
         #endregion
 
-        private System.Windows.Forms.Panel panelHeader;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Button btnProfileMenu;
-
-        public System.Windows.Forms.Label lblWelcome;
-        private System.Windows.Forms.Button btnApproveAccounts;
-        private System.Windows.Forms.Button btnApproveLoans;
-        private System.Windows.Forms.Button btnApproveCertificates;
-        private System.Windows.Forms.Button btnAddEmployee;
-
-        private System.Windows.Forms.ContextMenuStrip contextProfile;
-        private System.Windows.Forms.ToolStripMenuItem viewProfileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        public System.Windows.Forms.TextBox txtEmployeeID;
+        public System.Windows.Forms.TextBox txtName;
+        public System.Windows.Forms.TextBox txtEmail;
+        public System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
