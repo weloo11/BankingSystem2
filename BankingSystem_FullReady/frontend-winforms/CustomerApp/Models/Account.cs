2374bef49ceab501d397f93a470ca377a11ab93a
Mohamed-sameh <msameh343@gmail.com>
2025-12-04 19:36:06 +0200

    final inshallah

--- BankingSystem_FullReady/frontend-winforms/CustomerApp/Models/Account.cs
+++ BankingSystem_FullReady/frontend-winforms/CustomerApp/Models/Account.cs
@@ -1,12 +1,11 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace CustomerApp.Models
 {
-    [BsonIgnoreExtraElements]   // ignore any extra fields Mongo adds
     public class Account
     {
-        // MongoDB document id
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string _id { get; set; }
@@ -16,18 +15,29 @@ namespace CustomerApp.Models
         public string employeeID { get; set; }
         public double balance { get; set; }
         public string currency { get; set; }
-        public System.DateTime openDate { get; set; }
         public string accountType { get; set; }
         public string status { get; set; }
 
-        public CardInfo card { get; set; }
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime openDate { get; set; }
+
+        // ---------------------------
+        // EMBEDDED CARD OBJECT HERE
+        // ---------------------------
+        public Card card { get; set; }
     }
 
-    public class CardInfo
+    // ----------------------------------------
+    // CARD CLASS (Now inside Account.cs)
+    // ----------------------------------------
+    public class Card
     {
         public string cardNo { get; set; }
         public string pin { get; set; }
-        public System.DateTime expiryDate { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime expiryDate { get; set; }
+
         public string cvv { get; set; }
     }
 }
